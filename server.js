e3262e990f760861d007a8abc598181ce24928df
preetibana <preetibana995@gmail.com>
2020-07-29 20:22:21 +1000

    My Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+//Task 1:Run node as a web server for hosting static files
+
+var express= require('express');
+
+var app = express();
+
+let accounts= [
+{a:1, name:'Peter',deposit:5},
+{a:2, name:'George',deposit:15},
+{a:3, name:'Steve',deposit:50}
+
+]
+ app.get(function(req,res)
+ {
+
+    res.send('accounts');
+    res.write('accounts');
+ }
+ )
+ app.listen(4000);
+
+ 
+
+ //Port IP address
+var http = require('http')
+http.createServer(function(req,res)
+{
+res.write("Welcome Back Alien!!")
+res.end()
+
+}
+
+).listen(8080);
+
+
+node 
+ 
+
+// Task 2:Sum Of Two Numbers
+var a=7
+var b=8
+var c =a+b
+console.log("Sum Of Two Numbers is=" +c)
+
+
+
+
+
